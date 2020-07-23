61685b70c40d53dace3abce88ef80c7de28f5999
Vanh <tranvanh0905@gmail.com>
2020-07-23 08:52:16 +0700

    init

---
+++ src/components/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+      <h1>What is Vue</h1>
+      <p>
+        This is about page!!
+      </p>
+      <button class="btn btn-success">Add</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'About'
+  }
+</script>
+
+<style scoped>
+
+</style>
