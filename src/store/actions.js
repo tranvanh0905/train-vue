cb2fb22c3e79ab7d4594aef77893feaaf70be731
Vanh <tranvanh0905@gmail.com>
2020-07-27 13:15:40 +0700

    add vuex

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,23 +1,4 @@
-// export default {
-//     getTask({commit}, task) {
-//         commit('getTask', task)
-//     },
-//     addTask({commit}) {
-//         commit('addTask')
-//     },
-//     editTask({commit}, task) {
-//         commit('editTask', task)
-//     },
-//     removeTask({commit}, task) {
-//         commit('removeTask', task)
-//     },
-//     completeTask({commit}, task) {
-//         commit('completeTask', task)
-//     },
-//     clearTask({commit}) {
-//         commit('clearTask')
-//     }
-// }
+
 export default {
     getComment ({commit}, comment) {
         commit('getComment', comment)
